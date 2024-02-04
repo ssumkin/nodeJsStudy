a690cab2c4d5a504129cca1b2020652165a307a0
ssumkin <rms856@gmail.com>
2024-02-04 16:27:10 +0900

    db&Sequelize set

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require("express");
+const { sequelize } = require("./models");
 
 const app = express();
 
 app.set("port", 3000);
- 
-const indexRouter = require("./routes");
-const testRouter = require("./routes/test");
- 
-app.use("/", indexRouter);
- 
-app.use("/test", testRouter);
+
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("DB 연결 성공");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(app.get("port"), () => {
   console.log(`Server On Port ${app.get("port")}`);
